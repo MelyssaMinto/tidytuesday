27522dfffe82d238e4c9ab5d97aae6527608fee6
MelyssaMinto <37280368+MelyssaMinto@users.noreply.github.com>
2020-10-20 21:23:19 -0400

    update plots

--- 2020-10-20_beerAwards.R
+++ 2020-10-20_beerAwards.R
@@ -12,6 +12,8 @@ setwd("~/GitHub/tidytuesday")
 library(tidytuesdayR)
 library(tidyverse)
 library(maps)
+library(gganimate)
+library(transformr)
 
 # > reading in data -------------------------------------------------------
 
@@ -37,29 +39,33 @@ beer_awards %>%
   labs(y="NC Breweries", x= "Number of Medals", fill="Medal")+
   theme_minimal() +
   scale_fill_manual(values = c("Gold" = "#D4AF37", "Silver"= "#C0C0C0", "Bronze"= "#A97142"))+
-  ggtitle("Awards per Brewery in NC")
+  ggtitle("NC Beer Awards 1987 - 2020")+
   theme(text = element_text(family = "mono", size = 10),
         plot.background = element_rect(fill = "#FFF8E7", color = "transparent"),
         axis.title = element_text( face="bold"))
-  
+  ggsave("plots/2020-10-20_NCAwards.png")
 
 
 # want to see the medal count by state 
 MainStates$state = setNames(state.abb, state.name)[str_to_title(MainStates$region)]
 beer_awards %>% 
-  select(state, medal) %>% 
-  group_by(state) %>% 
+  select(state, medal, year) %>% 
+  group_by(state, year) %>% 
   summarize(n = n()) %>% 
   inner_join(MainStates) %>% 
   ggplot() +
   geom_polygon( data=MainStates, aes(x=long, y=lat, group = group), color="white", fill="grey", alpha=0.5 )+
   geom_polygon( aes(x=long, y=lat, group=group, fill = n), color="white", size = 0.2) +
-  ggtitle("number of beer awards per state")+
+  ggtitle("number of beer awards per state by year ")+
+  geom_text( aes(y=48, x=-80, label=as.character(year)),family='mono', check_overlap = TRUE, size=7, fontface="bold") +
+  #animate by year
+  transition_states(year,transition_length = 1,state_length = 3) +
   theme_minimal()+
-  theme(text = element_text(family = "mono", size = 10),
+  theme(aspect.ratio = .66,
+        text = element_text(family = "mono", size = 10),
         plot.background = element_rect(fill = "#FFF8E7", color = "transparent"),
         axis.title = element_text( face="bold"))
-
+anim_save("plots/2020-10-20_USAwards.gif")
 
 
 
